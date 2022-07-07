ef1860fc2d7817d5f8d2273e5cdb8428a8692ebc
BelladonnaEraes <alnison@mail.ru>
2022-07-07 22:09:31 +0300

    5

---
+++ script.js
@@ -0,0 +1,30 @@
+let arrProfiles = [
+{Foto: Images\Jhon.jpg, First name:"Jhon", Last name:"Dow", Occupation: "Doctor" Location:"USA", Age: 45, Gender: "Male", Followers: 1}
+{Foto: Images\Andre.jpg,	First name:"Andre", Last name:"Shaver", Occupation: "Fire prevention specialist" Location:"Australia", Age: 72, Gender: "Male", Followers: 3900}
+{Foto: Images\Sean.jpg,	First name:"Sean", Last name:"Diaz", Occupation: "School bus driver" Location:"Austria", Age: 42, Gender: "Male", Followers: 6780}
+{Foto: Images\Tim.jpg,	First name:"Tim", Last name:"Eisenberg", Occupation: "Brace maker" Location:"Belgium", Age: 32, Gender: "Male", Followers: 680}
+{Foto: Images\Dieter.jpg,	First name:"Dieter", Last name:"Daecher", Occupation: "Bindery machine setter" Location:"Brazil", Age: 55, Gender: "Male", Followers: 1183}
+{Foto: Images\Bernd.jpg,	First name:"Bernd", Last name:"Schmid", Occupation: "General practitioner" Location:"Canada", Age: 30, Gender: "Male", Followers: 7886}
+{Foto: Images\Paul.jpg,	First name:"Paul", Last name:"Lane", Occupation: "Card punching machine operator" Location:"Canada", Age: 43, Gender: "Male", Followers: 11187}
+{Foto: Images\Manuel.jpg,	First name:"Manuel", Last name:"Olson", Occupation: "Casino cage worker" Location:"Finland", Age: 23, Gender: "Male", Followers: 2280}
+{Foto: Images\Avenall.jpg,	First name:"Avenall", Last name:"Durepos", Occupation: "Midshipman" Location:"France", Age: 34, Gender: "Male", Followers: 830}
+{Foto: Images\Ermenegildo.jpg,	First name:"Ermenegildo", Last name:"Onio", Occupation: "Prosthetist" Location:"Italy", Age: 39, Gender: "Male", Followers: 83991}
+{Foto: Images\Valentin.jpg,	First name:"Valentin", Last name:"Macpherson", Occupation: "Bench technician" Location:"Poland", Age: 50, Gender: "Male", Followers: 7595}
+{Foto: Images\Rex.jpg,	First name:"Rex", Last name:"Tiedemann", Occupation: "Sociologist" Location:"South Africa", Age: 47, Gender: "Male", Followers: 3280}
+{Foto: Images\Jesse.jpg,	First name:"Jesse", Last name:"Morgan", Occupation: "Rural mail carrier" Location:"Tunisia", Age: 29, Gender: "Male", Followers: 7345}
+{Foto: Images\Roger.jpg,	First name:"Roger", Last name:"Humphries", Occupation: "Hydrographic surveyor" Location:"Uruguay", Age: 53, Gender: "Male", Followers: 74345}
+{Foto: Images\Don.jpg,	First name:"Don", Last name:"Schrader", Occupation: "Human resources clerk" Location:"Switzerland", Age: 44, Gender: "Male", Followers: 5445}
+{Foto: Images\Ashanti.jpg,	First name:"Ashanti", Last name:"Morris", Occupation: "Airline flight engineer" Location:"Australia", Age: 47, Gender: "Female", Followers: 5541}
+{Foto: Images\Jimmie.jpg,	First name:"Jimmie", Last name:"Harbuck", Occupation: "Compliance officer" Location:"Austria", Age: 25, Gender: "Female", Followers: 56882}
+{Foto: Images\Jayne.jpg,	First name:"Jayne", Last name:"Maguire", Occupation: "Library assistant" Location:"Brazil", Age: 39, Gender: "Female", Followers: 111}
+{Foto: Images\Stella.jpg,	First name:"Stella", Last name:"Scott", Occupation: "Bindery machine operator" Location:"Canada", Age: 71, Gender: "Female", Followers: 15611}
+{Foto: Images\Bonnie.jpg,	First name:"Bonnie", Last name:"Caban", Occupation: "Paper coating machine operator" Location:"Denmark", Age: 33, Gender: "Female", Followers: 156567}
+{Foto: Images\Laurel.jpg,	First name:"Laurel", Last name:"Dube", Occupation: "Cardiographer" Location:"Estonia", Age: 56, Gender: "Female", Followers: 6567}
+{Foto: Images\Jane.jpg,	First name:"Jane", Last name:"White", Occupation: "IT manager" Location:"France", Age: 22, Gender: "Female", Followers: 25956}
+{Foto: Images\Kelly.jpg,	First name:"Kelly", Last name:"Edwards", Occupation: "Baker" Location:"Greenland", Age: 43, Gender: "Female", Followers: 5477}
+{Foto: Images\Julie.jpg,	First name:"Julie", Last name:"Winter", Occupation: "Teacher assistant" Location:"Norway", Age: 58, Gender: "Female", Followers: 235790}
+{Foto: Images\Sierra.jpg,	First name:"Sierra", Last name:"Ford", Occupation: "Dividend clerk" Location:"Portugal", Age: 37, Gender: "Female", Followers: 34111}
+{Foto: Images\Mary.jpg,	First name:"Mary", Last name:"Brodersen", Occupation: "Lift truck operator" Location:"Switzerland", Age: 40, Gender: "Female", Followers: 35667}
+{Foto: Images\Marceline.jpg,	First name:"Marceline", Last name:"Guzman", Occupation: "Claims investigator" Location:"New Zealand", Age: 65, Gender: "Female", Followers: 534657}
+
+]
